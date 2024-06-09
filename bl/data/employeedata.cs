a266d9afb512252ef7838c742cd16cccbbb0eba4
Gerald Glen Dangcalan (GJAYZ) <74135168+gjayz099@users.noreply.github.com>
2024-06-09 16:29:28 +0800

    Add files via upload

---
+++ bl/data/employeedata.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace bl;
+
+public class employeedata
+{ 
+
+    public static async Task<(string err, List<bl.employee> data)> GetAllDataAsync()
+    {
+
+
+        string connectionString = $"Server=DESKTOP-79JOQKV;Database=PracticeData;User=user01;Password=admin;Trusted_Connection=false;TrustServerCertificate=True;";
+
+        List<bl.employee> ret = new List<bl.employee>();
+
+        string sql = "Select * From Employee";
+
+        string error = "";
+
+        try
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString)){
+                await connection.OpenAsync();
+
+
+                using(SqlCommand command = new SqlCommand(sql, connection))
+                {
+
+                   SqlDataReader reader =  await command.ExecuteReaderAsync();
+              
+                    while (await reader.ReadAsync())
+                    {
+                        ret.Add(new employee{ id = reader.GetGuid(0), name = reader.GetString(1), department = reader.GetString(2), salary = reader.GetString(3)});
+                    }    
+                };
+                await connection.CloseAsync();
+            }
+          
+        }
+        catch (Exception ex)
+        {
+            error = ex.Message;
+        }
+
+        return (error, ret);
+    }
+}
---
+++ bl/model/employee.cs
@@ -0,0 +1,9 @@
+﻿namespace bl;
+
+public class employee
+{
+    public Guid id { get; set; }
+    public string name { get; set; }
+    public string department { get; set; }
+    public string salary { get; set; }
+}
